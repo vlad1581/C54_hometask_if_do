04e47e66ea73e6433bf96fc6097c9de4748484c8
vlad1581 <vlad.art.91@mail.ru>
2022-01-27 00:51:34 +0300

    add Main.java to repo

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main (String []args ){
+        // распечатать все значения от 0 до 100
+        // с использованием функций for, do-while
+        int i;
+        int b=100;
+        for (i=0;i<100;i++) {
+            do {
+                i++;
+                System.out.println("Печатается значение   " + i);
+            } while (i<b);
+        }
+    }
+}
+
